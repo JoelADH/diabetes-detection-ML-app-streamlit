479e880aacd462bad16a5dc96429a06a5703401b
Joel Aday <47754704+JoelADH@users.noreply.github.com>
2026-02-23 11:18:34 +0000

    added sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,32 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# st.title("🎈 My new app")
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
+
+st.set_page_config(
+    page_title = "Diabetes risk prediction App",
+    layout="wide"
 )
+
+st.sidebar.title("Navegación")
+
+section = st.sidebar.radio(
+    "Ir a:",
+    (
+        "Introduction",
+        "EDA",
+        "models",
+        "demo",
+        "Conclusions"
+    )
+)
+
+st.sidebar.markdown("---")
+st.sidebar.info(
+    "ML project applied to prediction of "
+    "diabetes risk based on BRFSS 2015 dataset"
+)
+
+
